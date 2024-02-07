e77dd20f3efb241e22d5f2143a0102fcfc8ef44f
Bartosz Baran <bartosz.baran192@gmail.com>
2024-02-07 21:15:26 +0100

    configuration sets

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,18 @@
 import secrets
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+from config import config
+
+db = SQLAlchemy()
 
 
-def create_app():
-    secret_key = secrets.token_hex(16)
+def create_app(config_name):
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = secret_key
+    app.config.from_object(config[config_name])
+    config[config_name].init_app(app)
+
+    db.init_app(app)
 
     from .views import views
     app.register_blueprint(views, url_prefix='/')
